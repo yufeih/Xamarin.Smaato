09e8495d01124887b088aaa2a451f531bd220e63
Yufei Huang <yufeih@live.com>
2015-06-17 14:23:37 +0800

    Initial version.

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # Xamarin.Smaato
 
-Xamarin binding for Smaato mobile advertising platform, used by http://www.talktosomeone.net
+Xamarin binding for Smaato mobile advertising platform, used by http://talktosomeone.net
+
+## Getting Started
+
+Install Xamarin.Smaato from nuget:
+
+### PM> Install-Package Xamarin.Smaato 
+
+Add a BannarView to your app:
+
+```csharp
+BannerView banner = new BannerView(BaseContext);
+banner.AdSettings.PublisherId = 0;
+banner.AdSettings.AdspaceId = 0;
+banner.AsyncLoadNewBanner();
+banner.ScalingEnabled = false;
+banner.LocationUpdateEnabled = true;
+
+adContainer.AddView(banner);
+```
+
+## Build
+
+You need to set Xamarin to use JDK 1.7 to build this project
---
+++ Xamarin.Smatto.Test/MainActivity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Com.Smaato.Soma;
+
+namespace Xamarin.Smatto.Test
+{
+    [Activity(Label = "Xamarin.Smatto.Test", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            var group = FindViewById<ViewGroup>(Resource.Id.ad_container);
+
+            ShowSmaatoAdIn(group);
+
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+        }
+
+        private void ShowSmaatoAdIn(ViewGroup adContainer)
+        {
+            BannerView banner = new BannerView(BaseContext);
+            banner.AdSettings.PublisherId = 0;
+            banner.AdSettings.AdspaceId = 0;
+            banner.AsyncLoadNewBanner();
+            banner.ScalingEnabled = false;
+            banner.LocationUpdateEnabled = true;
+
+            adContainer.AddView(banner);
+        }
+    }
+}
+
