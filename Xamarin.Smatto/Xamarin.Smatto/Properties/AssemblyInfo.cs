af0412520e9eca6c003b347144705940ff27b5be
Yufei Huang <yufeih@live.com>
2015-06-17 12:13:36 +0800

    Add xamarin binding library

---
+++ Xamarin.Smatto/Xamarin.Smatto/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Xamarin.Smatto")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Studio Nine")]
+[assembly: AssemblyProduct("Xamarin.Smatto")]
+[assembly: AssemblyCopyright("Copyright © Studio Nine 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
